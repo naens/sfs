c8ee171017fa44e224c4bde5f16c21a0fef2211f
naens <naens@gmx.com>
2018-02-23 18:15:29 +0100

    add index area entry types

--- sfs.h
+++ sfs.h
@@ -1,6 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/* Index Data Area Entry Types */
+#define SFS_ENTRY_VOL_ID 0x01
+#define SFS_ENTRY_START 0x02
+#define SFS_ENTRY_UNUSED 0x10
+#define SFS_ENTRY_DIR 0x11
+#define SFS_ENTRY_FILE 0x12
+#define SFS_ENTRY_UNUSABLE 0x18
+#define SFS_ENTRY_DIR_DEL 0x19
+#define SFS_ENTRY_FILE_DEL 0x10
+
 /* The Super Block */
 struct S_SFS_SUPER {
     int64_t time_stamp;
