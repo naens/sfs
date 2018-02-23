de61622a030753b3aaf7f63254d6e8b946e87399
naens <naens@gmx.com>
2018-02-23 21:06:48 +0100

    sfs.h: add structures fot index area

--- sfs.h
+++ sfs.h
@@ -9,7 +9,10 @@
 #define SFS_ENTRY_FILE 0x12
 #define SFS_ENTRY_UNUSABLE 0x18
 #define SFS_ENTRY_DIR_DEL 0x19
-#define SFS_ENTRY_FILE_DEL 0x10
+#define SFS_ENTRY_FILE_DEL 0x1A
+
+#define DIR_NAME_LEN 53
+#define FILE_NAME_LEN 29
 
 /* The Super Block */
 struct S_SFS_SUPER {
@@ -22,6 +25,81 @@ struct S_SFS_SUPER {
     uint32_t rsvd_blocks;
     uint8_t block_size;
     uint8_t crc;
+};
+
+/* The Volume ID Entry */
+struct S_STS_VOL_ID {
+    uint8_t type;
+    uint8_t crc;
+    uint16_t resvd;
+    int64_t time_stamp;
+    uint8_t name[52];
+};
+
+/* The Start Marker Entry */
+struct S_STS_START {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t resvd[62];
+};
+
+/* The Unused Entry */
+struct S_STS_UNUSED {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t resvd[62];
+};
+
+/* The Directory Entry */
+struct S_STS_DIR {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t num_count;
+    int64_t time_stamp;
+    uint8_t name[DIR_NAME_LEN];
+};
+
+/* The File Entry */
+struct S_STS_FILE {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t num_count;
+    int64_t time_stamp;
+    uint64_t start_block;
+    uint64_t end_block;
+    uint64_t file_len;
+    uint8_t name[FILE_NAME_LEN];
+};
+
+/* The Unusable Entry */
+struct S_STS_UNUSABLE {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t resv0[8];
+    uint64_t start_block;
+    uint64_t end_block;
+    uint8_t resv1[38];
+};
+
+/* The Deleted Directory Entry */
+struct S_STS_DIR_DEL {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t num_count;
+    int64_t time_stamp;
+    uint8_t name[DIR_NAME_LEN];
+};
+
+/* The Deleted File Entry */
+struct S_STS_FILE_DEL {
+    uint8_t type;
+    uint8_t crc;
+    uint8_t num_count;
+    int64_t time_stamp;
+    uint64_t start_block;
+    uint64_t end_block;
+    uint64_t file_len;
+    uint8_t name[FILE_NAME_LEN];
 };
 
 struct S_SFS_SUPER *get_super(FILE *f);
